9d4f47fcf0a15891123f5eea4d11ce55cefdf63b
Ruso <Ruso@169.254.16.49>
2021-12-10 16:56:30 -0300

    Version 1

---
+++ src/telar/Telar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telar;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Ruso
+ */
+public class Telar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame=new Principal();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+}
